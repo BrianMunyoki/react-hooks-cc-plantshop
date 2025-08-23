226f766158030033d848939cb6304b3aa9cd413c
Brian <brianmunyoki168@gmail.com>
2025-08-24 00:06:38 +0300

    adding functions

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+ const [plants,setPlants]=useState([])
+  useEffect(()=>fetch (`http://localhost:6001/plants`)
+  .then(res=>res.json())
+  .then(data=>setPlants(data))
+  .catch(error=>console.error('Error:',error)));
+  function handleToggleStock(id){
+    const updatedPlants=plants.map(plant=>plant.id===id? {...plant, instock:!plant.instock}:plant)
+    setPlants(updatedPlants)
+  }
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants.map(plant=>{
+      return <PlantCard key={plant.id} name={plant.name} price={plant.cost}/>
+    })}</ul>
   );
 }
 
